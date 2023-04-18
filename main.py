748edef958e981fa469487a1f3412b239a7f461c
mfierroep <127438202+mfierroep@users.noreply.github.com>
2023-04-18 19:20:25 +0000

    Versao 1

--- main.py
+++ main.py
@@ -4,5 +4,21 @@
 
 f = open("sinalizacao.csv")
 
+
+antiga = ''
+primeira = True
+ausentes = []
 for line in f:
-  print(line.strip().split(';'))
+  if primeira:
+    primeira = False
+    continue
+  lista = line.strip().split(';')
+  if antiga == '' or antiga > lista[4]:
+    antiga = lista[4]
+  if lista[13].strip() == '' or lista[14].strip() == '':
+    ausentes.append(lista) 
+  #else:
+  #  print(lista)
+
+print(antiga)
+print(ausentes)
